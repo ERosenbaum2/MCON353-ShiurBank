9b5d950c3482289fa0adc94423f601f3a53c1162
mweiner8 <mweiner8@student.touro.edu>
2026-01-07 17:15:02 -0500

    deleting some unnecessary and unused files

--- app/src/main/java/springContents/SiteIndex.java
+++
@@ -1,7 +0,0 @@
-package springContents;
-
-// Spring Boot automatically serves index.html from src/main/resources/static/ at the root path "/"
-// No controller needed for static file serving
-public class SiteIndex {
-    // This class can be removed if not needed for future functionality
-}
